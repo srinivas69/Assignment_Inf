0c5b22d210cd1ecc527ca9d38f87a1c7463694e2
srinivas69 <srinivasmail.in@gmail.com>
2025-03-26 08:40:19 +0530

    first commit

---
+++ src/main/java/org/example/assignment_inf/AssignmentInfApplication.java
@@ -0,0 +1,13 @@
+package org.example.assignment_inf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentInfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentInfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assignment_inf/AssignmentInfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment_inf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentInfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
